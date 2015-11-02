5ac752306aa1354d90be2a80632f2b344aab1934
github username <mendelblesofsky@gmail.com>
2015-11-02 11:03:16 -0500

    some button changes and new audio files

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,12 @@
 class Movie < ActiveRecord::Base
 	has_many :quotes
+
+	def get_movies
+		movies = []
+		3.times do 
+ 			movies << Movie.all.sample.title
+		end 
+		movies << self.title
+		movies.shuffle!
+	end
 end
